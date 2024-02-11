4d5ac8f6c95cd7eaa4be98952dccf33fe2612e0a
Alexander Krauss <blogger.alex.fit@gmail.com>
2024-02-11 15:06:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// fetch HTML Elements
+let countEl = document.getElementById("count-el")
+let saveCount = document.getElementById("save")
+
+// Reisende zählen
+let count = 0
+function increment() {
+      count += 1
+      countEl.textContent = count
+}
+
+// Zwichenspeichern
+function save() {
+      let output = count + " -"
+      saveCount.textContent += output
+      count = 0
+      countEl.textContent = count
+}
